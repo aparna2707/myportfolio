96a7b7cc6dab5d211e863b161060ed31a38259a9
aparna2707 <mail4aparna.is@gmail.com>
2021-05-04 21:57:00 +0530

    adding my portfolio

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Photographer", "Rider",  "Designer", "Freelancer"],
+        strings: ["web developer", "teacher",  "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Photographer", "Rider",  "Designer", "Freelancer"],
+        strings: ["web developer", "teacher",  "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
